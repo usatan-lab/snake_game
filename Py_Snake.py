f28bdf7d2ca111b99c6067e890e43b18780ec6ee
usatan ✿‬ <usatan@usatannonotobukkukonpyuta.local>
2024-06-24 12:48:15 +0900

    update1

--- Py_Snake.py
+++ Py_Snake.py
@@ -6,12 +6,12 @@ pygame.init()
 
 # 色の定義
 white = (255, 255, 255)
-yellow = (255, 255, 103)
+yellow = (255, 255, 115)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
-blue = (50, 153, 213)
-wall_color = (255, 0, 0)  # 壁の色を赤色に設定
+blue = (50, 153, 215)
+wall_color = (250, 5, 5)  # 赤
 
 # 画面の大きさ
 dis_width = 800
@@ -19,11 +19,13 @@ dis_height = 600
 
 # 蛇のサイズ
 snake_block = 10
-initial_speed = 10
+initial_speed = 8
+
+wall_thickness = 10
 
 # 画面の設定
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Edureka')
+pygame.display.set_caption('Snake Game by usatan')
 
 clock = pygame.time.Clock()
 
@@ -48,6 +50,11 @@ def your_score(score):
     value = score_font.render('Your Score:'+ str(score),True,black)
     dis.blit(value,[0,0])
 
+def generate_food_position():
+    return (
+        round(random.randrange(wall_thickness,dis_width - wall_thickness - snake_block)/10.0) * 10.0,
+        round(random.randrange(wall_thickness, dis_height - wall_thickness - snake_block) / 10.0) * 10.0,
+    )
 def gameLoop():
     game_over = False
     game_close = False
@@ -61,15 +68,14 @@ def gameLoop():
     snake_List = []
     Length_of_snake = 1
 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    foodx, foody = generate_food_position()
 
     snake_speed = initial_speed
     score = 0
     while not game_over:
 
         while game_close:
-            dis.fill(blue)
+            dis.fill((0,0,0))
             message("You Lost! Press Q-Quit or C-Play Again", red)
             pygame.display.update()
 
@@ -98,12 +104,13 @@ def gameLoop():
                     y1_change = snake_block
                     x1_change = 0
 
-        if x1 >= dis_width - snake_block or x1 < snake_block or y1 >= dis_height - snake_block or y1 < snake_block:
+        if (x1 >= dis_width - wall_thickness - snake_block or x1 < wall_thickness or
+                y1 >= dis_height - wall_thickness - snake_block or y1 < wall_thickness):
             game_close = True
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
-        draw_walls()  # 壁を描画
+        draw_walls()
         pygame.draw.rect(dis, yellow, [foodx, foody, snake_block, snake_block])
         snake_Head = []
         snake_Head.append(x1)
@@ -122,8 +129,7 @@ def gameLoop():
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            foodx, foody = generate_food_position()
             Length_of_snake += 1
             score += 10
             snake_speed += 1
